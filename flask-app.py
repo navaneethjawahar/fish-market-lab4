6fc33cb5c321b5e7c39868576c85dcb8e562f2d6
Nav <navaneethjawahar333@gmail.com>
2022-07-22 14:08:02 -0400

    created a flask app for predicting the fish species

---
+++ flask-app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, request
+import pickle
+app = Flask(__name__,template_folder='templateFiles', static_folder='staticFiles')
+filename = 'model-files/finalized_model.pkl'
+loaded_model = pickle.load(open(filename, 'rb'))
+y_map = {0: 'Bream',
+ 1: 'Parkki',
+ 2: 'Perch',
+ 3: 'Pike',
+ 4: 'Roach',
+ 5: 'Smelt',
+ 6: 'Whitefish'}
+
+@app.route('/')
+def index():
+    return render_template(r'index.html')
+
+@app.route('/results', methods=['POST'])
+def results():
+    form = request.form.values()
+    if request.method == 'POST':
+        vals = [int(i) for i in list(form)]
+        print([vals])
+        predicted_Species = loaded_model.predict([vals])
+        species = y_map.get(predicted_Species[0])
+        print(predicted_Species)
+        print(species)
+        return render_template('resultsform.html', Species=species)
+
+
+if __name__=='__main__':
+    app.run(debug = True)
